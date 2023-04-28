59d33b65a6122b86d90cc9e969c2bde275e15a40
harikavanum5 <111125660+harikavanum5@users.noreply.github.com>
2023-04-28 19:52:09 +0530

    Update StepDefinition.java

--- src/test/java/stepDefinition/StepDefinition.java
+++ src/test/java/stepDefinition/StepDefinition.java
@@ -59,9 +59,9 @@ public void open_the_browser() throws Exception {
 	ScreenRecorderUtil.startRecord("open_the_browser");
 
 System.setProperty("webdriver.chrome.driver","C:\Users\hvanum\Downloads:\\chromedriver.exe");
-ChromeOptions options = new ChromeOptions();
-options.addArguments("--remote-allow-origins=*");
-WebDriver driver = new ChromeDriver(options);
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--remote-allow-origins=*");
+      driver = new ChromeDriver(options);
 
 
 
